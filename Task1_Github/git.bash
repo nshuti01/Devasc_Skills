34f9136e4432ea9378094ecf883888bbf6adf243
nshuti01 <55930674+nshuti01@users.noreply.github.com>
2022-01-25 07:44:40 +0100

    Add files via upload

---
+++ Task1_Github/git.bash
@@ -0,0 +1,7 @@
+echo "# git" >> README.md
+git init
+git add README.md
+git commit -m "git task"
+git branch -M Main
+git remote add origin https://github.com/nshuti01/Devasc_Skills.git
+git push -u origin Main
